52c0e71f10c024072450e435f580909ec21c6823
eliko <yyyeliko@gmail.com>
2019-08-11 16:52:36 +0300

    Create Echo Bot

--- index.js
+++ index.js
@@ -64,12 +64,9 @@ function kittenMessage(recipientId, text) {
 
     var theUrl = "https://poop2.azurewebsites.net/api/HttpTrigger1?code=CetbtwE9KeFOwaOtLtVUpSi6QiJGFFspjWwnbIOrL5SObgE5agWQQA==&name=young"
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    xmlHttp.send(null);
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 
     text = text || "";
     var values = text.split(' ');
