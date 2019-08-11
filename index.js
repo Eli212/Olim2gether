1dda49d42114053e1b314ecc3459fd52c05163d0
eliko <yyyeliko@gmail.com>
2019-08-11 14:03:27 +0300

    Create Echo Bot

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ app.post('/webhook', function(req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            # if (!kittenMessage(event.sender.id, event.message.text)) {
-            #    sendMessage(event.sender.id, { text: "Echo: " + event.message.text });
-            # }
+            // if (!kittenMessage(event.sender.id, event.message.text)) {
+            //    sendMessage(event.sender.id, { text: "Echo: " + event.message.text });
+            // }
 		anotherTest()
         } else if (event.postback) {
             console.log("Postback received: " + JSON.stringify(event.postback));
