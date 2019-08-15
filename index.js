2068421a67b513acfdea154d76290fb71dfdf8ed
tomer <tomermusafi@gmail.com>
2019-08-15 23:11:05 +0300

    make it better

--- index.js
+++ index.js
@@ -80,22 +80,21 @@ function checking_status(recipientId, text){
             console.log("------------"+ status)
             status.on('value', snapshot =>{
                 var finalStatus = snapshot.val();
-                console.log("kkkk: " + finalStatus)
+                console.log("kkkk: " + finalStatus);
                 switch(finalStatus){
-                case 0:
-                    sendMessage(recipientId, { text: "POOP" });
-                    console.log("pipi")
-                    finalStatus.set(1);
-//                    s.set(1);
-                    console.log(snapshot.val() + " " + "checking")
-
-                break;
-                case 1:
-                    sendMessage(recipientId, { text: "POOP" });
-                break;
-                default:
-
-            }
+                    case 0:
+                        sendMessage(recipientId, { text: "POOP" });
+                        console.log("pipi");
+                        finalStatus.set(1);
+    //                    s.set(1);
+                        console.log(snapshot.val() + " " + "checking")
+                        break;
+                    case 1:
+                        sendMessage(recipientId, { text: "POOP" });
+                        break;
+                    default:
+                        break;
+                }
             });
 
 
